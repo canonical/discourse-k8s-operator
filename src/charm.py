6b9dc1f889f4c715860a575d9a9b6c822997c52c
Jay Kuri <jay.kuri@canonical.com>
2020-07-14 14:59:06 -0600

    Update config process to be more testable. Add image_user and image_pass for private registries

--- src/charm.py
+++ src/charm.py
@@ -15,6 +15,109 @@ from ops.model import (
 )
 
 
+def create_discourse_pod_config(config):
+    pod_config = {
+        'DISCOURSE_POSTGRES_USERNAME': config['db_user'],
+        'DISCOURSE_POSTGRES_PASSWORD': config['db_password'],
+        'DISCOURSE_POSTGRES_HOST': config['db_host'],
+        'DISCOURSE_POSTGRES_NAME': config['db_name'],
+        'DISCOURSE_DEVELOPER_EMAILS': config['developer_emails'],
+        'DISCOURSE_HOSTNAME': config['external_hostname'],
+        'DISCOURSE_SMTP_DOMAIN': config['smtp_domain'],
+        'DISCOURSE_SMTP_ADDRESS': config['smtp_address'],
+        'DISCOURSE_SMTP_PORT': config['smtp_port'],
+        'DISCOURSE_SMTP_AUTHENTICATION': config['smtp_authentication'],
+        'DISCOURSE_SMTP_OPENSSL_VERIFY_MODE': config['smtp_openssl_verify_mode'],
+        'DISCOURSE_SMTP_USER_NAME': config['smtp_username'],
+        'DISCOURSE_SMTP_PASSWORD': config['smtp_password'],
+        'DISCOURSE_REDIS_HOST': config['redis_host'],
+    }
+    return pod_config
+
+
+def create_ingress_config(app_name, config):
+    ingressResource = {
+        "name": app_name + "-ingress",
+        "spec": {
+            "rules": [
+                {
+                    "host": config['external_hostname'],
+                    "http": {
+                        "paths": [
+                            {
+                                "path": "/",
+                                "backend": {
+                                    "serviceName": app_name,
+                                    "servicePort": 3000
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    }
+    return ingressResource
+
+
+def get_pod_spec(app_name, config):
+    pod_spec = {
+        "version": 3,
+        "containers": [{
+            "name": app_name, 
+            "imageDetails": {"imagePath": config['discourse_image']},
+            "imagePullPolicy": "IfNotPresent",
+            "ports": [{
+                "containerPort": 3000,
+                "protocol": "TCP",
+            }],
+            "envConfig": create_discourse_pod_config(config),
+            "kubernetes": {
+                "readinessProbe": {
+                    "httpGet": {
+                        "path": "/srv/status",
+                        "port": 3000,
+                    }
+                }
+            },
+        }],
+        "kubernetesResources": {
+            "ingressResources": [
+                create_ingress_config(app_name, config)
+            ]
+        }
+    }
+    # This handles when we are trying to get an image from a private
+    # registry.
+    if config['image_user'] and config['image_pass']:
+        pod_spec['containers'][0]['imageDetails'].set("username", config['image_user'])
+        pod_spec['containers'][0]['imageDetails'].set("password", config['image_pass'])
+
+    return pod_spec
+
+
+def check_for_config_problems(config):
+    errors = []
+    missing_fields = check_for_missing_config_fields(config)
+
+    if len(missing_fields):
+        errors.append('Required configuration missing: {}'.format(" ".join(missing_fields)))
+
+    return errors
+
+
+def check_for_missing_config_fields(config):
+    missing_fields = []
+
+    needed_fields = ['db_user', 'db_password', 'db_host', 'db_name', 'smtp_address',
+                     'redis_host']
+    for key in needed_fields:
+        if len(config[key]) == 0:
+            missing_fields.append(key)
+
+    return sorted(missing_fields)
+
+
 class DiscourseCharm(CharmBase):
     state = StoredState()
 
@@ -26,109 +129,14 @@ class DiscourseCharm(CharmBase):
         self.framework.observe(self.on.config_changed, self.configure_pod)
         self.framework.observe(self.on.upgrade_charm, self.configure_pod)
 
-    def get_pod_spec(self):
-
-        # get our config
-        config = self.framework.model.config
-
-        pod_spec = {
-            "version": 3,
-            "containers": [{
-                "name": self.framework.model.app.name,
-                "imageDetails": {"imagePath": config['discourse_image']},
-                "imagePullPolicy": "IfNotPresent",
-                "ports": [{
-                    "containerPort": 3000,
-                    "protocol": "TCP",
-                }],
-                "config": self.create_discourse_pod_config(config),
-                "kubernetes": {
-                    "readinessProbe": {
-                        "httpGet": {
-                            "path": "/srv/status",
-                            "port": 3000,
-                        }
-                    }
-                },
-            }],
-            "kubernetesResources": {
-                "ingressResources": [
-                    self.create_ingress_config(config)
-                ]
-            }
-        }
-
-        # This handles when we are trying to get an image from a private
-        # registry. Details are here:
-        # https://kubernetes.io/docs/concepts/containers/images/#specifying-imagepullsecrets-on-a-pod
-        if config['registry_secrets_name']:
-            pod_spec['containers'][0].set("imagePullSecrets", config['registry_secrets_name'])
-
-        return pod_spec
-
-    def create_discourse_pod_config(self, config):
-        pod_config = {
-            'DISCOURSE_POSTGRES_USERNAME': config['db_user'],
-            'DISCOURSE_POSTGRES_PASSWORD': config['db_password'],
-            'DISCOURSE_POSTGRES_HOST': config['db_host'],
-            'DISCOURSE_POSTGRES_NAME': config['db_name'],
-            'DISCOURSE_DEVELOPER_EMAILS': config['developer_emails'],
-            'DISCOURSE_HOSTNAME': config['external_hostname'],
-            'DISCOURSE_SMTP_DOMAIN': config['smtp_domain'],
-            'DISCOURSE_SMTP_ADDRESS': config['smtp_address'],
-            'DISCOURSE_SMTP_PORT': config['smtp_port'],
-            'DISCOURSE_SMTP_AUTHENTICATION': config['smtp_authentication'],
-            'DISCOURSE_SMTP_OPENSSL_VERIFY_MODE': config['smtp_openssl_verify_mode'],
-            'DISCOURSE_SMTP_USER_NAME': config['smtp_username'],
-            'DISCOURSE_SMTP_PASSWORD': config['smtp_password'],
-            'DISCOURSE_REDIS_HOST': config['redis_host'],
-        }
-        return pod_config
-
-    def create_ingress_config(self, config):
-        ingressResource = {
-            "name": self.framework.model.app.name + "-ingress",
-            "spec": {
-                "rules": [
-                    {
-                        "host": config['external_hostname'],
-                        "http": {
-                            "paths": [
-                                {
-                                    "path": "/",
-                                    "backend": {
-                                        "serviceName": self.framework.model.app.name,
-                                        "servicePort": 3000
-                                    }
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
-        }
-
-        return ingressResource
-
-    def check_config_is_valid(self):
-        config = self.framework.model.config
+    def check_config_is_valid(self, config):
         valid_config = True
-        errors = []
-        missing_fields = []
-
-        needed_fields = ['db_user', 'db_password', 'db_host', 'db_name', 'smtp_address',
-                         'redis_host']
-        for key in needed_fields:
-            if len(config[key]) == 0:
-                missing_fields.append(key)
-
-        if len(missing_fields):
-            valid_config = False
-            errors.append('Required configuration missing: {}'.format(" ".join(missing_fields)))
+        errors = check_for_config_problems(config)
 
         # set status if we have a bad config
         if errors:
             self.model.unit.status = BlockedStatus(", ".join(errors))
+            valid_config = False
         else:
             self.model.unit.status = MaintenanceStatus("Configuration passed validation")
 
@@ -142,8 +150,9 @@ class DiscourseCharm(CharmBase):
         # Leader must set the pod spec.
         if self.model.unit.is_leader():
             # Get our spec definition.
-            if self.check_config_is_valid():
-                pod_spec = self.get_pod_spec()
+            if self.check_config_is_valid(self.framework.model.config):
+                # Get pod spec using our app name and config
+                pod_spec = get_pod_spec(self.framework.model.app.name, self.framework.model.config)
                 # Set our pod spec.
                 self.model.pod.set_spec(pod_spec)
 
