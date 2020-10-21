09cc41b990631a04bfd33b51f83e3f63f308610b
Jay Kuri <jay.kuri@canonical.com>
2020-10-21 08:48:22 -0600

    Updates with Docstrings, renamed fixture files and various tweaks to charm and tests per most recent round of review.

--- src/charm.py
+++ src/charm.py
@@ -7,13 +7,14 @@ from ops.charm import CharmBase
 from ops.main import main
 from ops.framework import StoredState
 
-from ops.model import MaintenanceStatus, BlockedStatus, ActiveStatus
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus
 
 
 pgsql = ops.lib.use("pgsql", 1, "postgresql-charmers@lists.launchpad.net")
 
 
 def create_discourse_pod_config(config):
+    """Create the pod environment config from the juju config."""
     pod_config = {
         'DISCOURSE_POSTGRES_USERNAME': config['db_user'],
         'DISCOURSE_POSTGRES_PASSWORD': config['db_password'],
@@ -36,6 +37,7 @@ def create_discourse_pod_config(config):
 
 
 def create_ingress_config(app_name, config):
+    """Create the ingress config form the juju config."""
     annotations = {}
     ingressResource = {
         "name": app_name + "-ingress",
@@ -61,6 +63,13 @@ def create_ingress_config(app_name, config):
 
 
 def get_pod_spec(app_name, config):
+    """Get the entire pod spec using the juju config.
+
+    - uses create_discourse_pod_config() to generate pod envConfig.
+
+    - uses create_ingress_config() to generate pod ingressResources.
+
+    """
     pod_spec = {
         "version": 3,
         "containers": [
@@ -92,10 +101,16 @@ def get_pod_spec(app_name, config):
 
 
 def check_for_config_problems(config):
+    """Check if there are issues with the juju config.
+
+    - Primarily looks for missing config options using check_for_missing_config_fields()
+
+    - Returns a list of errors if any were found.
+    """
     errors = []
     missing_fields = check_for_missing_config_fields(config)
 
-    if len(missing_fields):
+    if missing_fields:
         errors.append('Required configuration missing: {}'.format(" ".join(missing_fields)))
 
     if "db_host" not in config or config["db_host"] is None:
@@ -105,6 +120,11 @@ def check_for_config_problems(config):
 
 
 def check_for_missing_config_fields(config):
+    """Check for missing fields in juju config.
+
+    - Returns a list of required fields that are either not present
+      or are empty.
+    """
     missing_fields = []
 
     needed_fields = [
@@ -125,13 +145,18 @@ def check_for_missing_config_fields(config):
 
 
 class DiscourseCharm(CharmBase):
-    state = StoredState()
+    stored = StoredState()
 
     def __init__(self, *args):
+        """Initialization.
+
+        - Primarily sets up defaults and event handlers.
+
+        """
         super().__init__(*args)
 
         # TODO: is_started is unused. Remove?
-        self.state.set_default(is_started=False, db_user=None, db_password=None, db_host=None)
+        self.stored.set_default(is_started=False, db_user=None, db_password=None, db_host=None)
         self.framework.observe(self.on.leader_elected, self.configure_pod)
         self.framework.observe(self.on.config_changed, self.configure_pod)
         self.framework.observe(self.on.upgrade_charm, self.configure_pod)
@@ -141,11 +166,17 @@ class DiscourseCharm(CharmBase):
         self.framework.observe(self.db.on.master_changed, self.on_database_changed)
 
     def check_config_is_valid(self, config):
+        """Check that the provided config is valid.
+
+        - Returns True if config is valid, False otherwise.
+
+        - Sets model status as appropriate.
+        """
         valid_config = True
         errors = check_for_config_problems(config)
 
-        # set status if we have a bad config
-        if len(errors) > 0:
+        # Set status if we have a bad config.
+        if errors:
             self.model.unit.status = BlockedStatus(", ".join(errors))
             valid_config = False
         else:
@@ -154,6 +185,12 @@ class DiscourseCharm(CharmBase):
         return valid_config
 
     def configure_pod(self, event=None):
+        """Configure pod.
+
+        - Verifies config is valid and unit is leader.
+
+        - Configures pod using pod_spec generated from config.
+        """
         # Set our status while we get configured.
         self.model.unit.status = MaintenanceStatus('Configuring pod')
 
@@ -165,37 +202,49 @@ class DiscourseCharm(CharmBase):
             config = dict(self.model.config)
             if not config["db_name"]:
                 config["db_name"] = self.app.name
-            config["db_user"] = self.state.db_user
-            config["db_password"] = self.state.db_password
-            config["db_host"] = self.state.db_host
+            config["db_user"] = self.stored.db_user
+            config["db_password"] = self.stored.db_password
+            config["db_host"] = self.stored.db_host
             # Get our spec definition.
             if self.check_config_is_valid(config):
                 # Get pod spec using our app name and config
                 pod_spec = get_pod_spec(self.framework.model.app.name, config)
                 # Set our pod spec.
                 self.model.pod.set_spec(pod_spec)
-                self.state.is_started = True
+                self.stored.is_started = True
                 self.model.unit.status = ActiveStatus()
         else:
-            self.state.is_started = True
+            self.stored.is_started = True
             self.model.unit.status = ActiveStatus()
 
     def on_database_relation_joined(self, event):
+        """Event handler for a newly joined database relation.
+
+        - Sets the event.database field on the database joined event.
+
+        - Required because setting the database name is only possible
+          from inside the event handler per https://github.com/canonical/ops-lib-pgsql/issues/2
+        """
         # Per https://github.com/canonical/ops-lib-pgsql/issues/2,
         # changing the setting in the config will not take effect,
         # unless the relation is dropped and recreated.
         event.database = self.model.config["db_name"]
 
     def on_database_changed(self, event):
+        """Event handler for database relation change.
+
+        - Sets our database parameters based on what was provided
+          in the relation event.
+        """
         if event.master is None:
-            self.state.db_user = None
-            self.state.db_password = None
-            self.state.db_host = None
+            self.stored.db_user = None
+            self.stored.db_password = None
+            self.stored.db_host = None
             return
 
-        self.state.db_user = event.master.user
-        self.state.db_password = event.master.password
-        self.state.db_host = event.master.host
+        self.stored.db_user = event.master.user
+        self.stored.db_password = event.master.password
+        self.stored.db_host = event.master.host
 
         self.configure_pod()
 
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -3,42 +3,41 @@
 # Copyright 2020 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import os
+import copy
 import glob
+import mock
+import os
 import unittest
 import yaml
-import mock
-import copy
-from types import SimpleNamespace
-from pprint import pprint
 
-from charm import DiscourseCharm, create_discourse_pod_config, get_pod_spec, check_for_missing_config_fields
+from types import SimpleNamespace
+
+from charm import (
+    check_for_missing_config_fields,
+    create_discourse_pod_config,
+    get_pod_spec,
+    DiscourseCharm,
+)
 
 from ops import testing
 
-dirname = os.path.dirname(__file__)
 
-
-# Valid and Invalid configs are present in the fixtures
-# directory. The files contain the juju config, along with
-# the spec that that config should produce in the case of
-# valid configs. In the case of invalid configs, they contain
-# the juju config and the error that should be triggered by
-# the config. These scenarios are tested below. Additional
-# config variations can be created by creating an appropriately
-# named config file in the fixtures directory. Valid configs
-# should be named: config_valid_###.yaml and invalid configs
-# should be named: config_invalid_###.yaml.
-#
-# This function loads the configs for use by the tests.
 def load_configs(directory):
+    """Load configs for use by tests.
+
+    Valid and invalid configs are present in the fixtures directory. The files
+    contain the juju config, along with the spec that that config should
+    produce in the case of valid configs. In the case of invalid configs, they
+    contain the juju config and the error that should be triggered by the
+    config. These scenarios are tested below. Additional config variations can
+    be created by creating an appropriately named config file in the fixtures
+    directory. Valid configs should be named: config_valid_###.yaml and invalid
+    configs should be named: config_invalid_###.yaml."""
     configs = {}
     for filename in glob.glob(os.path.join(directory, 'config*.yaml')):
-        pprint(filename)
         with open(filename) as file:
-            name = os.path.splitext(os.path.basename(filename))
-            pprint(name[0])
-            configs[name[0]] = yaml.full_load(file)
+            name, _ = os.path.splitext(os.path.basename(filename))
+            configs[name] = yaml.full_load(file)
     return configs
 
 
@@ -48,7 +47,7 @@ class TestDiscourseK8sCharmHooksDisabled(unittest.TestCase):
         self.harness.disable_hooks()
         self.harness.set_leader(True)
         self.harness.begin()
-        self.configs = load_configs(os.path.join(dirname, 'fixtures'))
+        self.configs = load_configs(os.path.join(os.path.dirname(__file__), 'fixtures'))
 
     def test_valid_configs_are_ok(self):
         """Test that a valid config is considered valid."""
@@ -77,7 +76,8 @@ class TestDiscourseK8sCharmHooksDisabled(unittest.TestCase):
                 )
 
     def test_charm_config_process(self):
-        test_config = copy.deepcopy(self.configs['config_valid_1']['config'])
+        """Test that the entire config process for the charm works."""
+        test_config = copy.deepcopy(self.configs['config_valid_complete']['config'])
         test_config['db_user'] = 'discourse_m'
         test_config['db_password'] = 'a_real_password'
         test_config['db_host'] = '10.9.89.237'
@@ -86,28 +86,29 @@ class TestDiscourseK8sCharmHooksDisabled(unittest.TestCase):
         db_event.master.user = 'discourse_m'
         db_event.master.password = 'a_real_password'
         db_event.master.host = '10.9.89.237'
-        expected_spec = get_pod_spec(self.harness.charm.framework.model.app.name, test_config)
-        self.harness.update_config(self.configs['config_valid_1']['config'])
+        expected_spec = (get_pod_spec(self.harness.charm.framework.model.app.name, test_config), None)
+        self.harness.update_config(self.configs['config_valid_complete']['config'])
         self.harness.charm.on_database_relation_joined(db_event)
         self.harness.charm.on_database_changed(db_event)
         configured_spec = self.harness.get_pod_spec()
-        self.maxDiff = None
-        self.assertEqual(configured_spec[0], expected_spec, 'Configured spec does not match expected pod spec.')
+        self.assertEqual(configured_spec, expected_spec, 'Configured spec does not match expected pod spec.')
 
     def test_charm_config_process_not_leader(self):
+        """Test that the config process on a non-leader does not trigger a pod_spec change."""
         non_leader_harness = testing.Harness(DiscourseCharm)
         non_leader_harness.disable_hooks()
         non_leader_harness.set_leader(False)
         non_leader_harness.begin()
 
         action_event = mock.Mock()
-        non_leader_harness.update_config(self.configs['config_valid_1']['config'])
+        non_leader_harness.update_config(self.configs['config_valid_complete']['config'])
         non_leader_harness.charm.configure_pod(action_event)
         result = non_leader_harness.get_pod_spec()
         self.assertEqual(result, None, 'Non-leader does not set pod spec.')
 
     def test_lost_db_relation(self):
-        self.harness.update_config(self.configs['config_valid_1']['config'])
+        """Test that losing our DB relation triggers a drop of pod config."""
+        self.harness.update_config(self.configs['config_valid_complete']['config'])
         db_event = SimpleNamespace()
         db_event.master = None
         self.harness.charm.on_database_changed(db_event)
