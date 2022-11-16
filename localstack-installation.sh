795651073dd9904cc743d120e4f6e4bf55c04e81
Grégory Schiano <gregory.schiano@canonical.com>
2022-11-16 14:33:24 +0000

    Try accessing localstack through docker bridge

--- localstack-installation.sh
+++ localstack-installation.sh
@@ -4,7 +4,7 @@ pip install pip --upgrade
 pip install pyopenssl --upgrade
 pip install localstack # install LocalStack cli
 docker pull localstack/localstack # Make sure to pull the latest version of the image
-localstack start -d # Start LocalStack in the background
+EDGE_BIND_HOST=0.0.0.0 localstack start -d # Start LocalStack in the background (binding to all host ip)
 echo "Waiting for LocalStack startup..." # Wait 30 seconds for the LocalStack container
 localstack wait -t 30 # to become ready before timing out 
 echo "Startup complete"
