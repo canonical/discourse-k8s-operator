92394383544d97ad6b169ab4d824588a3363a36c
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2023-03-17 12:18:48 +0100

    Refactor integration tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -3,31 +3,24 @@
 # See LICENSE file for licensing details.
 """Discourse integration tests."""
 
-import json
 import logging
 import re
 import socket
 import unittest.mock
-from typing import Dict, Optional
-from urllib.parse import urlencode, urlparse
+from typing import Dict
+from urllib.parse import urlparse
 
 import pytest
 import requests
 import urllib3.exceptions
 from boto3 import client
 from botocore.config import Config
-from bs4 import BeautifulSoup
 from ops.model import ActiveStatus, Application
 from pytest_operator.plugin import Model, OpsTest
 from requests.adapters import HTTPAdapter, Retry
 
-from charm import PROMETHEUS_PORT, SERVICE_NAME, SERVICE_PORT
-from tests.integration.helpers import (
-    DBInfo,
-    get_db_info,
-    get_discourse_email_token_hash,
-    get_unit_address,
-)
+from charm import PROMETHEUS_PORT, SERVICE_PORT
+from tests.integration.helpers import get_unit_address
 
 from . import types
 
@@ -87,167 +80,24 @@ async def test_prom_exporter_is_up(app: Application):
 @pytest.mark.asyncio
 @pytest.mark.abort_on_fail
 async def test_setup_discourse(
-    ops_test: OpsTest, app: Application, app_config: Dict[str, str], requests_timeout: float
+    app_config: Dict[str, str],
+    requests_timeout: float,
+    discourse_address: str,
 ):
-    """Check discourse if working properly by registrating an Admin and generates an API
-    Note that the API is later used to manipulate Discourse
-    """
+    """Check discourse servers the registration page."""
 
-    discourse_address = await get_unit_address(ops_test, app.name)
-    discourse_url = f"http://{discourse_address}:{SERVICE_PORT}"
     session = requests.session()
-
     logger.info("Getting registration page")
-
     # Send request to bootstrap page and set Host header to app_name (which the application
     # expects)
     response = session.get(
-        f"{discourse_url}/finish-installation/register",
+        f"{discourse_address}/finish-installation/register",
         headers={"Host": f"{app_config['external_hostname']}"},
         timeout=requests_timeout,
         allow_redirects=True,
     )
 
     assert response.status_code == 200
-
-    # Parse output and send registration form
-    parsed_registration: BeautifulSoup = BeautifulSoup(response.content, features="html.parser")
-
-    # Get the form info
-    assert parsed_registration.body
-    logger.info("Submitting registration form")
-
-    response = session.post(
-        f"{discourse_url}/finish-installation/register",
-        headers={
-            "Host": f"{app_config['external_hostname']}",
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        data=urlencode(
-            {
-                "utf8": parsed_registration.body.find(
-                    "input", attrs={"name": "utf8"}  # type: ignore
-                ).get("value"),
-                "authenticity_token": parsed_registration.body.find(
-                    "input", attrs={"name": "authenticity_token"}  # type: ignore
-                ).get("value"),
-                "username": "admin",
-                "email": app_config["developer_emails"],
-                "password": "MyLovelySecurePassword2022!",
-            }
-        ),
-        allow_redirects=False,
-        timeout=requests_timeout,
-    )
-
-    # Replies with a redirect
-    assert response.status_code == 302
-
-    # Fecth the email token from DB
-    postgresql_info: Optional[DBInfo] = await get_db_info(app, SERVICE_NAME)
-    assert postgresql_info is not None
-    email_token = await get_discourse_email_token_hash(
-        postgresql_info, app_config["developer_emails"]
-    )
-    assert email_token is not None
-
-    # Confirm email with token
-    session.cookies.clear()
-
-    logger.info("Getting account activation page")
-
-    response = session.get(
-        f"{discourse_url}/u/password-reset//{email_token}",
-        headers={"Host": f"{app_config['external_hostname']}"},
-        timeout=requests_timeout,
-    )
-
-    assert response.status_code == 200
-
-    # Parse the response to get the authenticity_token
-    parsed_validation: BeautifulSoup = BeautifulSoup(response.content, features="html.parser")
-
-    logger.info("Getting challenge for account activation form")
-
-    # There's a challenge to get through an Ajax request to submit the activation
-    response = session.get(
-        f"{discourse_url}/session/hp",
-        headers={
-            "Host": f"{app_config['external_hostname']}",
-            "X-Requested-With": "XMLHttpRequest",
-        },
-        timeout=requests_timeout,
-    )
-
-    assert response.status_code == 200
-
-    assert parsed_validation.body
-    form_fields = {
-        "_method": "put",
-        "authenticity_token": parsed_validation.body.find(
-            "input", attrs={"name": "authenticity_token"}  # type: ignore
-        ).get("value"),
-        "password_confirmation": response.json()["value"],
-        # The challenge string is reversed see
-        # https://github.com/discourse/discourse/blob/main/app/assets/javascripts/discourse/scripts/activate-account.js
-        "challenge": response.json()["challenge"][::-1],
-    }
-
-    logger.info("Submitting account validation form")
-
-    # Submit the activation of the account
-    response = session.post(
-        f"{discourse_url}/u/activate-account/{email_token}",
-        headers={
-            "Host": f"{app_config['external_hostname']}",
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        data=urlencode(form_fields),
-        allow_redirects=False,
-        timeout=requests_timeout,
-    )
-
-    assert response.status_code == 200
-
-    logger.info("Getting admin page")
-
-    # Load Admin page
-    response = session.get(
-        f"{discourse_url}/admin/",
-        headers={
-            "Host": f"{app_config['external_hostname']}",
-            # Without the user-agent, the server answer doesn't contain csrf
-            "User-Agent": "AppleWebKit/537.36",
-        },
-        timeout=requests_timeout,
-    )
-
-    # Extract the CSRF token
-    parsed_admin: BeautifulSoup = BeautifulSoup(response.content, features="html.parser")
-    assert parsed_admin.head
-
-    csrf_token = parsed_admin.head.find("meta", attrs={"name": "csrf-token"}).get(  # type: ignore
-        "content"
-    )
-
-    logger.info("Getting admin API key")
-
-    # Finally create an API Key, which will be used on the next integration tests
-    response = session.post(
-        f"{discourse_url}/admin/api/keys",
-        headers={
-            "Host": f"{app_config['external_hostname']}",
-            "X-Requested-With": "XMLHttpRequest",
-            "X-CSRF-Token": csrf_token,  # type: ignore
-            "Content-Type": "application/json",
-        },
-        data=json.dumps({"key": {"description": "Key to The Batmobile", "username": "admin"}}),
-        timeout=requests_timeout,
-    )
-
-    assert response.status_code == 200
-
-    logger.info("Admin API Key: %s", {response.json()["key"]["key"]})
 
 
 @pytest.mark.asyncio
