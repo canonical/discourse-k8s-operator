c037480edda5fefbb941e8a257695f4ccdb4127f
Arturo Enrique Seijas Fernandez <arturo.seijas@canonical.com>
2023-05-29 14:46:40 +0200

    Use dummy series jammy for postgresql

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -134,7 +134,7 @@ async def app_fixture(
     """
     # Deploy relations to speed up overall execution
     await asyncio.gather(
-        model.deploy("postgresql-k8s", channel="latest/stable", series="jammy"),
+        model.deploy("postgresql-k8s", channel="latest/stable", series="jammy", trust=True),
         model.deploy("redis-k8s", series="focal"),
         model.deploy("nginx-ingress-integrator", series="focal", trust=True),
     )
