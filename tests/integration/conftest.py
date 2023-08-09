b3a1b1b6ea1c8972e6ba6dddce653fcf3ff71aae
Niels Robin-Aubertin <git@niels.fr>
2023-08-09 14:37:27 -0400

    Try rev116 for postgres

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -135,7 +135,11 @@ async def app_fixture(
     # Deploy relations to speed up overall execution
 
     postgres_app = await model.deploy(
-        "postgresql-k8s", channel="14/edge", series="jammy", trust=True
+        "postgresql-k8s",
+        channel="14/edge",
+        series="jammy",
+        revision=116,
+        trust=True
     )
     await model.wait_for_idle(apps=[postgres_app.name], status="active")
 
