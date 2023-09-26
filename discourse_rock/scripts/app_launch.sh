362c1f28cdb03d23b8f9d781b34f258a49d0ac26
Niels Robin-Aubertin <git@niels.fr>
2023-09-26 15:37:24 -0400

    Fix app_launch script

--- discourse_rock/scripts/app_launch.sh
+++ discourse_rock/scripts/app_launch.sh
@@ -6,9 +6,4 @@ export UNICORN_BIND_ALL=0.0.0.0
 export UNICORN_SIDEKIQS=1
 
 cd "$CONTAINER_APP_ROOT/app" || exit
-
-bin/unicorn -c config/unicorn.conf.rb &
-
-# If one of the processes exits, the other one will be killed so that the pod will be restarted by the failing probes
-wait -n
-kill "$(jobs -p)"
+exec bin/unicorn -c config/unicorn.conf.rb
