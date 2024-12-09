ebdbb2d50b82a6a509a996cbe3820d910b12dee6
ali ugur <ali.ugur@canonical.com>
2024-12-09 06:33:19 +0300

    Chore(): Lint

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -526,12 +526,17 @@ def test_sidekiq_env_variable():
 
     harness.set_can_connect(CONTAINER_NAME, True)
     harness.container_pebble_ready(CONTAINER_NAME)
-    plan_before_set_config = harness.get_container_pebble_plan(CONTAINER_NAME).services["discourse"].environment
+    plan_before_set_config = (
+        harness.get_container_pebble_plan(CONTAINER_NAME).services["discourse"].environment
+    )
     harness.update_config({"sidekiq_max_memory": 500})
-    plan_after_set_config = harness.get_container_pebble_plan(CONTAINER_NAME).services["discourse"].environment
+    plan_after_set_config = (
+        harness.get_container_pebble_plan(CONTAINER_NAME).services["discourse"].environment
+    )
     assert plan_before_set_config != plan_after_set_config
     assert "1000" in plan_before_set_config["UNICORN_SIDEKIQ_MAX_RSS"]
     assert "500" in plan_after_set_config["UNICORN_SIDEKIQ_MAX_RSS"]
+
 
 def test_handle_pebble_ready_event():
     """
