f2e0a7124c350fc4dec8a13977896e12bb3cce69
Ben Hoyt <ben.hoyt@canonical.com>
2025-04-10 17:49:43 +1200

    hmmm, it seems pytest_addoption does have to live here

---
+++ tests/conftest.py
@@ -0,0 +1,35 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+"""Module for test customizations."""
+
+
+def pytest_addoption(parser):
+    """Adds parser switches."""
+    parser.addoption("--discourse-image", action="store")
+    parser.addoption("--localstack-address", action="store")
+    parser.addoption("--saml-email", action="store")
+    parser.addoption("--saml-password", action="store")
+    parser.addoption("--charm-file", action="store", default=None)
+    parser.addoption(
+        "--use-existing",
+        action="store_true",
+        default=False,
+        help="This will skip deployment of the charms. Useful for local testing.",
+    )
+    parser.addoption(
+        "--keep-models",
+        action="store_true",
+        default=False,
+        help="keep temporarily-created models",
+    )
+    parser.addoption(
+        "--model",
+        action="store",
+        help="Juju model to use; if not provided, a new model "
+        "will be created for each test which requires one",
+    )
+
+
+def pytest_configure(config):
+    """Adds config options."""
+    config.addinivalue_line("markers", "abort_on_fail")
